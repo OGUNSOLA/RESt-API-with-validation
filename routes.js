66c1dc596fc00254e775118daa1abbf39c337ecf
OLUBUKOLA OGUNSOLA <olubukola@me.com>
2021-08-31 16:49:11 -0400

    first commit

---
+++ routes.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const bcrypt = require('bcrypt');
+const {check, validationResult} = require('express-validator');
+
+// This array is used to keep track of user records
+// as they are created.
+const users = [];
+
+// Construct a router instance.
+const router = express.Router();
+
+// Route that returns a list of users.
+router.get('/users', (req, res) => {
+  res.json(users);
+});
+
+// Route that creates a new user.
+router.post('/users', (req, res) => {
+  // Get the user from the request body.
+  const user = req.body;
+
+  // Add the user to the `users` array.
+  users.push(user);
+
+  // Set the status to 201 Created and end the response.
+  res.status(201).end();
+});
+
+module.exports = router;
